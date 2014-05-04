a7594563773d7c2b551151e70915f8fba9667458
ItsJonQ <itsjonq@gmail.com>
2014-05-03 23:43:10 -0400

    Fixed version in package and bower

--- src/kreate.js
+++ src/kreate.js
@@ -1,11 +1,3 @@
-/*!
- * Kreate.js
- * A tiny element creator plugin for jQuery.
- * v0.0.1 (https://github.com/ItsJonQ/kreate)
- * Copyright 2014 Jon Q
- * Licensed under MIT (https://github.com/itsjonq/kreate/blob/master/LICENSE)
- */
-
 (function ($, undefined) { "use strict";
 
     // Return false if jQuery is not defined or valid
