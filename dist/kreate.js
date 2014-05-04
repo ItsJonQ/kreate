23699a92616e68b3b61530b93265120235a473d4
ItsJonQ <itsjonq@gmail.com>
2014-05-03 22:39:39 -0400

    Started building kreate.js

--- dist/kreate.js
+++ dist/kreate.js
@@ -6,8 +6,31 @@
  * Licensed under MIT (https://github.com/itsjonq/kreate/blob/master/LICENSE)
  */
 
-(function ($, undefined) { 'use strict';
+(function ($, undefined) { "use strict";
 
+    // Return false if jQuery is not defined or valid
+    if(!$ || typeof jQuery !== "function") {
+        return false;
+    }
 
+    var kreate;
+
+    // Defining kreate
+    kreate = function(options) {
+
+        /**
+         * settings
+         * These are the default settings/options for the plugin
+         *
+         * @type { object }
+         */
+        var settings = $.extend({
+
+        }, options);
+
+    };
+
+    // Adding kreate to jQuery
+    jQuery.kreate = kreate;
 
 })(jQuery);
--- src/kreate.js
+++ src/kreate.js
@@ -1,5 +1,28 @@
-(function ($, undefined) { 'use strict';
+(function ($, undefined) { "use strict";
 
+    // Return false if jQuery is not defined or valid
+    if(!$ || typeof jQuery !== "function") {
+        return false;
+    }
 
+    var kreate;
+
+    // Defining kreate
+    kreate = function(options) {
+
+        /**
+         * settings
+         * These are the default settings/options for the plugin
+         *
+         * @type { object }
+         */
+        var settings = $.extend({
+
+        }, options);
+
+    };
+
+    // Adding kreate to jQuery
+    jQuery.kreate = kreate;
 
 })(jQuery);
